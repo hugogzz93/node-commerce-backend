d9633476a46ed5a282d33cb129414087f8206546
Hugo González <pinelo93@gmail.com>
2019-01-24 12:39:22 -0600

    Adds fileManagement with minio

---
+++ minio.js
@@ -0,0 +1,12 @@
+import * as Minio from 'minio'
+
+console.log(Minio)
+
+const minioClient = new Minio.Client({
+    endPoint: '127.0.0.1',
+    port: 9000,
+    useSSL: false,
+    accessKey: 'AKIAIOSFODNN7EXAMPLE',
+    secretKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+});
+export { minioClient }
